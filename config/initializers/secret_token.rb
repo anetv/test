dbee640a7820b0a7a426f76ba29cfb4284d262f8
Karl Stolley <karl.stolley@gmail.com>
2012-05-15 21:17:06 -0500

    Fresh Rails app for the morning portion of the workshop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cwcon::Application.config.secret_token = 'f4ce665f92fd74a6f8d6629f18599b1780cc37796e221740cb76a79b0dbcb3f2f833f4eaf1c65d69a50d5ee23f5c025da25605fc03a94053be74f067fb5a0331'
